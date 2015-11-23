bbe0e58443062460d0b523cb323d311d235b1ecf
Caiit <Caitlin.Lagrand@live.nl>
2015-11-23 18:18:03 +0100

    added distance to ball

--- background_removal.py
+++ background_removal.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
 
+pixToMeters = 19.98
+
 # Read the video
 vid = cv2.VideoCapture("football/rolling_ball.avi")
+
+# f = cv2.imread("football/image1.jpg")
+# height, width = f.shape[:2]
+# f = cv2.resize(f, (width/2, height/2) )
 while(vid.isOpened()):
     _, f = vid.read()
 
@@ -10,10 +16,9 @@ while(vid.isOpened()):
     hsv = cv2.cvtColor(f,cv2.COLOR_BGR2HSV)
     green = cv2.inRange(hsv,np.array((53,100, 100)),np.array((90,255,255)))
     s = green[:,1:]
-    im = cv2.GaussianBlur(s, (13,13), 0, 0)
+    im = cv2.GaussianBlur(s, (23,23), 0, 0)
 
     edges = cv2.Canny(im,60,180,apertureSize = 3,  L2gradient=True)
-    torgb = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
 
     contours,hierarchy = cv2.findContours(edges,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(edges, contours, -1, (255,0,0), 3)
@@ -30,20 +35,30 @@ while(vid.isOpened()):
 
     # Filter by Area.
     params.filterByArea = True
-    params.minArea = 100
+    params.minArea = 120
 
     detector = cv2.SimpleBlobDetector(params)
 
     # Detect blobs.
     keypoints = detector.detect(edges)
+    if len(keypoints) > 0:
+        print keypoints[0].pt[0]
+        print keypoints[0].pt[1]
+        print keypoints[0].size
+
+        sizeBall = keypoints[0].size
+        distance = (pixToMeters / sizeBall)
+        print distance
+        print "-------------"
 
     # Draw detected blobs as blue circles.
     # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-    im_with_keypoints = cv2.drawKeypoints(edges, keypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    im_with_keypoints = cv2.drawKeypoints(f, keypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 
     cv2.imshow('detect ball',im_with_keypoints)
-    
+    # cv2.imshow('edges',edges)
+
     if cv2.waitKey(100) & 0xFF == ord('q'):
     	break
 
